21fc9f7618e8d2f5313441372b13befb05cf8ab2
T-x-T <37040316+T-x-T@users.noreply.github.com>
2024-12-31 18:40:05 +0100

    finish implementing basics of Location module

--- backend/src/location/test.rs
+++ backend/src/location/test.rs
@@ -9,8 +9,32 @@ mod get_low_speed_connections {
 			let low_speed_connections = loc.get_low_speed_connections();
 
 			for connection in low_speed_connections {
-				println!("comparing {loc:?} to {connection:?}");
+				println!("comparing low speed connection from {loc:?} to {connection:?}");
 				assert!(connection.get_low_speed_connections().contains(&loc));
+			}
+		}
+	}
+
+	#[test]
+	fn all_high_speed_connections_fit() {
+		for loc in Location::get_iter() {
+			let high_speed_connections = loc.get_high_speed_connections();
+
+			for connection in high_speed_connections {
+				println!("comparing high speed connection from {loc:?} to {connection:?}");
+				assert!(connection.get_high_speed_connections().contains(&loc));
+			}
+		}
+	}
+
+	#[test]
+	fn all_plane_connections_fit() {
+		for loc in Location::get_iter() {
+			let plane_connections = loc.get_plane_connections();
+
+			for connection in plane_connections {
+				println!("comparing plane speed connection from {loc:?} to {connection:?}");
+				assert!(connection.get_plane_connections().contains(&loc));
 			}
 		}
 	}
